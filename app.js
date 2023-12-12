eae08a083f5be23a070860b190e573df6b465eaa
ZZ <shadow1088punch@gmail.com>
2023-12-12 09:48:04 +0100

    mmh~

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var handlebars = require('express-handlebars');
+const fs = require("fs");
+
 
 //--------------------------------------------------------------------Inicializace modulů pro routy--------------------------------------------------------------------//
 
@@ -55,8 +57,9 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  //res.status(err.status || 500);
+  //res.render('error');¨
+  res.send(fs.readFileSync("./views/error.html", "utf8"))
 });
 
 module.exports = app;
