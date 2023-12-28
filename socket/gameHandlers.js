ba6f2fd1e4aa6b565ed8e6c05c225278876ecd96
Antoš Michael <antosmichael07@gmail.com>
2023-12-28 21:53:37 +0100

    only number code

--- socket/gameHandlers.js
+++ socket/gameHandlers.js
@@ -23,7 +23,7 @@ module.exports = {
 
     socket.on("game:gen", () => {
       var code = "";
-      var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      var possible = "0123456789";
 
       for (var i = 0; i < 8; i++) {
         code += possible.charAt(Math.floor(Math.random() * possible.length));
