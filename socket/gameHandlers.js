d50945849e30e93bbbd9badec0e763a0a6934046
ZZ <shadow1088punch@gmail.com>
2023-12-28 21:57:27 +0100

    6digit room code

--- socket/gameHandlers.js
+++ socket/gameHandlers.js
@@ -25,7 +25,7 @@ module.exports = {
       var code = "";
       var possible = "0123456789";
 
-      for (var i = 0; i < 8; i++) {
+      for (var i = 0; i < 6; i++) {
         code += possible.charAt(Math.floor(Math.random() * possible.length));
       }
       games[code] = new Pexeso();
